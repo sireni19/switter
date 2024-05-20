ef7152856faca863e44226994a269941b1e76bc9
Michele <pro100mishqa63@gmail.com>
2024-05-20 21:44:06 +0300

    Добавлена SecurityConfig

---
+++ src/main/java/by/prokopovich/switter/security/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package by.prokopovich.switter.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    /**
+     * Отключена защита от CSRF-атак, STATELESS - приложение не будет создавать и управлять сессиями пользователей,
+     * а вместо этого будет использовать JWT аутентификацию, Customizer.withDefaults() - механизм базовой HTTP-аутентификации
+     * пользователь в заголовках запроса будет предоставлять свои учетные данные
+     *
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/accounts/register").permitAll()
+                        .requestMatchers("/error").permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
